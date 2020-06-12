f3be03c63cede90a8b5b183c8129cc7bd8f97919
rehammuzzamil <reham.muzzamil@venturedive.com>
2020-06-12 19:50:58 +0500

    10 : Add plan and task domain classes

--- src/main/java/org/smartregister/domain/Expression.java
+++ src/main/java/org/smartregister/domain/Expression.java
@@ -5,14 +5,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.Builder;
+import org.smartregister.domain.Action.SubjectConcept;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Builder
 public class Expression {
     private String name;
     private String language;
     private String expression;
+    private SubjectConcept subjectConcept;
 }
--- src/main/java/org/smartregister/domain/Trigger.java
+++ src/main/java/org/smartregister/domain/Trigger.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.Builder;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Builder
 public class Trigger {
     private String type;
     private String name;
