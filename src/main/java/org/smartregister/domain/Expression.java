6c8be40513834f119dd1bd473c501646a5db00e3
rehammuzzamil <reham.muzzamil@venturedive.com>
2020-06-12 12:32:03 +0500

    10 - Move Plan and Task domain and dependent classes to OpenSRP Plan evaluator

---
+++ src/main/java/org/smartregister/domain/Expression.java
@@ -0,0 +1,18 @@
+package org.smartregister.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Expression {
+    private String name;
+    private String language;
+    private String expression;
+}
