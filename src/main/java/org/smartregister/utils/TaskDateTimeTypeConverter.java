85f6fdaa9b7fdf2650118e5d070a49d4d70b4aec
rehammuzzamil <reham.muzzamil@venturedive.com>
2020-06-15 16:44:31 +0500

    Moved Client, Event and Task domain classes and their dependencies.

---
+++ src/main/java/org/smartregister/utils/TaskDateTimeTypeConverter.java
@@ -0,0 +1,36 @@
+package org.smartregister.utils;
+
+import com.google.gson.*;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.lang.reflect.Type;
+
+public class TaskDateTimeTypeConverter extends DateTimeTypeConverter {
+
+	private DateTimeFormatter dateTimeFormatter;
+
+	public TaskDateTimeTypeConverter() {
+		this("yyyy-MM-dd'T'HHmm");
+	}
+
+	public TaskDateTimeTypeConverter(String dateFormat) {
+		dateTimeFormatter = DateTimeFormat.forPattern(dateFormat);
+	}
+
+	@Override
+	public DateTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+			throws JsonParseException {
+		try {
+			return dateTimeFormatter.parseDateTime(json.getAsString());
+		} catch (IllegalArgumentException e) {
+			return new DateTime(json.getAsString());
+		}
+	}
+
+	@Override
+	public JsonElement serialize(DateTime src, Type typeOfSrc, JsonSerializationContext context) {
+		return new JsonPrimitive(src.toString(dateTimeFormatter));
+	}
+}
