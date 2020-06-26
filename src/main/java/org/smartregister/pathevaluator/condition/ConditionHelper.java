c495aedd157b728b40e666b3d03f9bffbb92f470
Samuel Githengi <samuel.githengi@gmail.com>
2020-06-26 12:47:32 +0300

    skip evaluation of condition if not applicable to trigger

--- src/main/java/org/smartregister/pathevaluator/condition/ConditionHelper.java
+++ src/main/java/org/smartregister/pathevaluator/condition/ConditionHelper.java
@@ -5,10 +5,12 @@ package org.smartregister.pathevaluator.condition;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.smartregister.domain.Action;
 import org.smartregister.domain.Action.SubjectConcept;
 import org.smartregister.domain.Condition;
 import org.smartregister.pathevaluator.PathEvaluatorLibrary;
+import org.smartregister.pathevaluator.TriggerType;
 import org.smartregister.pathevaluator.action.ActionHelper;
 
 import com.ibm.fhir.model.resource.Resource;
@@ -32,15 +34,23 @@ public class ConditionHelper {
 	 * 
 	 * @param resource the resource being evaluated against
 	 * @param action the action being evaluated
-	 * @param planIdentifier 
+	 * @param planIdentifier
+	 * @param triggerEvent
 	 * @return result of condition evaluation
 	 */
-	public boolean evaluateActionConditions(Resource resource, Action action, String planIdentifier) {
+	public boolean evaluateActionConditions(Resource resource, Action action, String planIdentifier,
+	        TriggerType triggerEvent) {
 		boolean isValid = true;
 		for (Condition condition : action.getCondition()) {
+			//this condition is not applicable for this triggering event, skip its evaluation
+			if (StringUtils.isNotBlank(condition.getExpression().getReference())
+			        && triggerEvent.getValue().equalsIgnoreCase(condition.getExpression().getReference())) {
+				continue;
+			}
 			SubjectConcept concept = condition.getExpression().getSubjectConcept();
 			if (concept != null) {
-				List<? extends Resource> resources = actionHelper.getConditionSubjectResources(condition, action, resource,planIdentifier);
+				List<? extends Resource> resources = actionHelper.getConditionSubjectResources(condition, action, resource,
+				    planIdentifier);
 				if (resources != null) {
 					isValid = resources.stream().anyMatch(
 					    r -> pathEvaluatorLibrary.evaluateBooleanExpression(r, condition.getExpression().getExpression()));
