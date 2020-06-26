71837f29a896802faf9a83b94e51c303e21cfa49
Samuel Githengi <samuel.githengi@gmail.com>
2020-06-26 17:24:20 +0300

    Unit test condition is skipped if trigger is different

--- src/test/java/org/smartregister/pathevaluator/condition/ConditionHelperTest.java
+++ src/test/java/org/smartregister/pathevaluator/condition/ConditionHelperTest.java
@@ -5,6 +5,8 @@ package org.smartregister.pathevaluator.condition;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 import java.util.Collections;
@@ -20,6 +22,7 @@ import org.mockito.Mock;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.stubbing.Answer;
+import org.powermock.reflect.Whitebox;
 import org.smartregister.domain.Action;
 import org.smartregister.domain.Action.SubjectConcept;
 import org.smartregister.domain.Condition;
@@ -43,6 +46,9 @@ public class ConditionHelperTest {
 	@Mock
 	private ActionHelper actionHelper;
 	
+	@Mock
+	private PathEvaluatorLibrary pathEvaluatorLibrary;
+	
 	private Patient patient;
 	
 	private Action action;
@@ -51,11 +57,11 @@ public class ConditionHelperTest {
 	
 	private Expression expression;
 	
-	private String plan=UUID.randomUUID().toString();
+	private String plan = UUID.randomUUID().toString();
 	
 	@BeforeClass
 	public static void bootstrap() {
-		PathEvaluatorLibrary.init(null,null,null, null);
+		PathEvaluatorLibrary.init(null, null, null, null);
 	}
 	
 	@Before
@@ -70,36 +76,53 @@ public class ConditionHelperTest {
 	
 	@Test
 	public void testEvaluateActionWithEmptyConditions() {
-		assertTrue(conditionHelper.evaluateActionConditions(patient, action,plan,TriggerType.PLAN_ACTIVATION));
+		assertTrue(conditionHelper.evaluateActionConditions(patient, action, plan, TriggerType.PLAN_ACTIVATION));
 	}
 	
 	@Test
 	public void testEvaluateActionConditionWithoutSubject() {
 		action.getCondition().add(condition);
-		assertTrue(conditionHelper.evaluateActionConditions(patient, action,plan,TriggerType.PLAN_ACTIVATION));
+		assertTrue(conditionHelper.evaluateActionConditions(patient, action, plan, TriggerType.PLAN_ACTIVATION));
 		
 		expression = Expression.builder().expression("Patient.name.given = 'Jil'").build();
 		condition = Condition.builder().kind("applicability").expression(expression).build();
 		action.getCondition().add(condition);
-		assertFalse(conditionHelper.evaluateActionConditions(patient, action,null,TriggerType.PLAN_ACTIVATION));
+		assertFalse(conditionHelper.evaluateActionConditions(patient, action, null, TriggerType.PLAN_ACTIVATION));
 	}
 	
 	@Test
 	public void testEvaluateActionConditionWithSubject() {
 		condition = condition.toBuilder()
-		        .expression(
-		            expression.toBuilder().subjectConcept(new SubjectConcept("Task")).expression("$this.contained.where(Task.code.text='MDA_Round_1' and Task.businessStatus.text='Completed').exists()").build())
+		        .expression(expression.toBuilder().subjectConcept(new SubjectConcept("Task")).expression(
+		            "$this.contained.where(Task.code.text='MDA_Round_1' and Task.businessStatus.text='Completed').exists()")
+		                .build())
 		        .build();
 		action.getCondition().add(condition);
-		assertFalse(conditionHelper.evaluateActionConditions(patient, action,plan,TriggerType.PLAN_ACTIVATION));
+		assertFalse(conditionHelper.evaluateActionConditions(patient, action, plan, TriggerType.PLAN_ACTIVATION));
 		
-		when(actionHelper.getConditionSubjectResources(condition, action, patient,plan)).thenAnswer(new Answer<List<Task>>() {
-			
-			@Override
-			public List<Task> answer(InvocationOnMock invocation) throws Throwable {
-				return Collections.singletonList(TestData.createTask());
-			}
-		});
-		assertTrue(conditionHelper.evaluateActionConditions(patient, action,plan,TriggerType.PLAN_ACTIVATION));
+		when(actionHelper.getConditionSubjectResources(condition, action, patient, plan))
+		        .thenAnswer(new Answer<List<Task>>() {
+			        
+			        @Override
+			        public List<Task> answer(InvocationOnMock invocation) throws Throwable {
+				        return Collections.singletonList(TestData.createTask());
+			        }
+		        });
+		assertTrue(conditionHelper.evaluateActionConditions(patient, action, plan, TriggerType.PLAN_ACTIVATION));
+		verify(actionHelper).getConditionSubjectResources(condition, action, patient, plan);
+	}
+	
+	@Test
+	public void testEvaluateActionConditionSkippedIfDifferentTrigger() {
+		Whitebox.setInternalState(conditionHelper, "pathEvaluatorLibrary", pathEvaluatorLibrary);
+		condition = condition.toBuilder().expression(expression.toBuilder()
+		        .reference(TriggerType.EVENT_SUBMISSION.getValue()).subjectConcept(new SubjectConcept("Task"))
+		        .expression(
+		            "$this.contained.where(Task.code.text='MDA_Round_1' and Task.businessStatus.text='Completed').exists()")
+		        .build()).build();
+		action.getCondition().add(condition);
+		assertTrue(conditionHelper.evaluateActionConditions(patient, action, plan, TriggerType.PLAN_ACTIVATION));
+		verifyNoInteractions(pathEvaluatorLibrary);
+		
 	}
 }
