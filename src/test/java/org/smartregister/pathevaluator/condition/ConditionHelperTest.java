55b20542f38e42e03041bae3f0a23838a637f0f5
Samuel Githengi <samuel.githengi@gmail.com>
2020-06-26 12:59:34 +0300

    Update unit tests

--- src/test/java/org/smartregister/pathevaluator/condition/ConditionHelperTest.java
+++ src/test/java/org/smartregister/pathevaluator/condition/ConditionHelperTest.java
@@ -26,6 +26,7 @@ import org.smartregister.domain.Condition;
 import org.smartregister.domain.Expression;
 import org.smartregister.pathevaluator.PathEvaluatorLibrary;
 import org.smartregister.pathevaluator.TestData;
+import org.smartregister.pathevaluator.TriggerType;
 import org.smartregister.pathevaluator.action.ActionHelper;
 
 import com.ibm.fhir.model.resource.Patient;
@@ -69,18 +70,18 @@ public class ConditionHelperTest {
 	
 	@Test
 	public void testEvaluateActionWithEmptyConditions() {
-		assertTrue(conditionHelper.evaluateActionConditions(patient, action,plan));
+		assertTrue(conditionHelper.evaluateActionConditions(patient, action,plan,TriggerType.PLAN_ACTIVATION));
 	}
 	
 	@Test
 	public void testEvaluateActionConditionWithoutSubject() {
 		action.getCondition().add(condition);
-		assertTrue(conditionHelper.evaluateActionConditions(patient, action,plan));
+		assertTrue(conditionHelper.evaluateActionConditions(patient, action,plan,TriggerType.PLAN_ACTIVATION));
 		
 		expression = Expression.builder().expression("Patient.name.given = 'Jil'").build();
 		condition = Condition.builder().kind("applicability").expression(expression).build();
 		action.getCondition().add(condition);
-		assertFalse(conditionHelper.evaluateActionConditions(patient, action,null));
+		assertFalse(conditionHelper.evaluateActionConditions(patient, action,null,TriggerType.PLAN_ACTIVATION));
 	}
 	
 	@Test
@@ -90,7 +91,7 @@ public class ConditionHelperTest {
 		            expression.toBuilder().subjectConcept(new SubjectConcept("Task")).expression("Task.code.text='MDA_Round_1' and Task.businessStatus.text='Completed' ").build())
 		        .build();
 		action.getCondition().add(condition);
-		assertFalse(conditionHelper.evaluateActionConditions(patient, action,plan));
+		assertFalse(conditionHelper.evaluateActionConditions(patient, action,plan,TriggerType.PLAN_ACTIVATION));
 		
 		when(actionHelper.getConditionSubjectResources(condition, action, patient,plan)).thenAnswer(new Answer<List<Task>>() {
 			
@@ -99,6 +100,6 @@ public class ConditionHelperTest {
 				return Collections.singletonList(TestData.createTask());
 			}
 		});
-		assertTrue(conditionHelper.evaluateActionConditions(patient, action,plan));
+		assertTrue(conditionHelper.evaluateActionConditions(patient, action,plan,TriggerType.PLAN_ACTIVATION));
 	}
 }
