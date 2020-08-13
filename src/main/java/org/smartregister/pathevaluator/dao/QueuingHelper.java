e348aeb1df149420141faf2f964797a57f67f219
Reham Muzzamil <reham.muzzamil@venturedive.com>
2020-08-13 17:17:18 +0500

    286 : Add method in QueuingHelper class

--- src/main/java/org/smartregister/pathevaluator/dao/QueuingHelper.java
+++ src/main/java/org/smartregister/pathevaluator/dao/QueuingHelper.java
@@ -1,9 +1,14 @@
 package org.smartregister.pathevaluator.dao;
 
+import com.ibm.fhir.model.resource.QuestionnaireResponse;
+import org.smartregister.domain.Action;
 import org.smartregister.pathevaluator.TriggerType;
 
 public interface QueuingHelper {
 
 	void addToQueue(String planId, TriggerType triggerType, String locationId);
+
+	void addToQueue(String resource, QuestionnaireResponse questionnaireResponse,
+					Action action, String planIdentifier, String jurisdictionCode, TriggerType triggerEvent);
 
 }
