4c8e3a66dea87f60e01687099f4f7f0ebab4f389
Samuel Githengi <samuel.githengi@gmail.com>
2020-09-30 12:53:50 +0300

    Generate getters and setters using lombok

--- src/main/java/org/smartregister/domain/PhysicalLocation.java
+++ src/main/java/org/smartregister/domain/PhysicalLocation.java
@@ -3,9 +3,11 @@ package org.smartregister.domain;
 import java.io.Serializable;
 import java.util.Set;
 
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode(of = "id")
+@Data
 public class PhysicalLocation implements Serializable {
 
 	private static final long serialVersionUID = -4863877528673921296L;
@@ -23,67 +25,5 @@ public class PhysicalLocation implements Serializable {
 	private Set<LocationTag> locationTags;
 	
 	private transient boolean isJurisdiction;
-	
-	public String getType() {
-		return type;
-	}
-	
-	public void setType(String type) {
-		this.type = type;
-	}
-	
-	public String getId() {
-		return id;
-	}
-	
-	public void setId(String id) {
-		this.id = id;
-	}
-	
-	public Geometry getGeometry() {
-		return geometry;
-	}
-	
-	public void setGeometry(Geometry geometry) {
-		this.geometry = geometry;
-	}
-	
-	public LocationProperty getProperties() {
-		return properties;
-	}
-	
-	public void setProperties(LocationProperty properties) {
-		this.properties = properties;
-	}
-	
-	public Long getServerVersion() {
-		return serverVersion;
-	}
-	
-	public void setServerVersion(Long serverVersion) {
-		this.serverVersion = serverVersion;
-	}
-	
-	public boolean isJurisdiction() {
-		return isJurisdiction;
-	}
-	
-	public void setJurisdiction(boolean isJurisdiction) {
-		this.isJurisdiction = isJurisdiction;
-	}
-	
-	public Set<LocationTag> getLocationTags() {
-		return locationTags;
-	}
-	
-	public void setLocationTags(Set<LocationTag> locationTags) {
-		this.locationTags = locationTags;
-	}
-	
-	@Override
-	public String toString() {
-		return "PhysicalLocation [type=" + type + ", id=" + id + ", geometry=" + geometry + ", properties=" + properties
-		        + ", serverVersion=" + serverVersion + ", locationTags=" + locationTags + "]";
-	}
 	
 }
