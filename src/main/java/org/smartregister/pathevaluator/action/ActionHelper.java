394614d0d706c890603a56afeb92cff9208f1cec
Ephraim Kigamba <ekigamba@ona.io>
2021-04-06 17:29:39 +0300

    Revert changes on GLOBAL_TASK

--- src/main/java/org/smartregister/pathevaluator/action/ActionHelper.java
+++ src/main/java/org/smartregister/pathevaluator/action/ActionHelper.java
@@ -82,9 +82,8 @@ public class ActionHelper {
 				return clientDao.findFamilyMemberyByJurisdiction(jurisdiction.getCode());
 
 			case TASK:
-				return taskDao.findTasksByJurisdiction(jurisdiction.getCode(),planIdentifier);
 			case GLOBAL_TASK:
-				return taskDao.findTasksByJurisdiction(jurisdiction.getCode());
+				return taskDao.findTasksByJurisdiction(jurisdiction.getCode(),planIdentifier);
 
 			case DEVICE:
 				return stockDao.findInventoryItemsInAJurisdiction(jurisdiction.getCode());
