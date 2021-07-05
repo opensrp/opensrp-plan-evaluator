4ebdbf35670b9a0f9bae48a03becd6155b04cbae
bennsimon <botieno@ona.io>
2021-07-05 10:02:06 +0300

    update test && update snapshot version

--- src/test/java/org/smartregister/converters/PhysicalLocationAndStocksConverterTest.java
+++ src/test/java/org/smartregister/converters/PhysicalLocationAndStocksConverterTest.java
@@ -2,6 +2,9 @@ package org.smartregister.converters;
 
 import com.google.gson.Gson;
 import com.ibm.fhir.model.resource.Bundle;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.junit.Test;
 import org.smartregister.domain.PhysicalLocationAndStocks;
 import org.smartregister.pathevaluator.PathEvaluatorLibrary;
@@ -12,7 +15,12 @@ import static org.junit.Assert.assertTrue;
 
 public class PhysicalLocationAndStocksConverterTest {
 
-	private static String physicalLocationAndStocksJsonObject = "{\"stocks\":[{\"type\":\"Stock\",\"serverVersion\":1521112142815,\"identifier\":15,\"transaction_type\":\"Inventory\",\"providerid\":\"3c11c3ad-c366-443f-8ca8-c8c21f5bdd22\",\"value\":10,\"version\":1606748740346,\"deliveryDate\":\"2020-06-06\",\"accountabilityEndDate\":\"2020-07-02T10:00:00.000+0000\",\"donor\":\"ADB\",\"serialNumber\":\"1234serial\",\"locationId\":\"90398\",\"customProperties\":{\"PO Number\":\"111\",\"UNICEF section\":\"Health\"},\"id\":\"05934ae338431f28bf6793b241b3cd6f\",\"date_created\":\"1520892000000\",\"_rev\":\"v1\"},{\"type\":\"Stock\",\"serverVersion\":1521112142815,\"identifier\":1,\"transaction_type\":\"Inventory\",\"providerid\":\"3c11c3ad-c366-443f-8ca8-c8c21f5bdd22\",\"value\":10,\"version\":1606748740346,\"deliveryDate\":\"2020-06-06\",\"accountabilityEndDate\":\"2020-07-02T10:00:00.000+0000\",\"donor\":\"ADB\",\"serialNumber\":\"1234serial\",\"locationId\":\"90398\",\"customProperties\":{\"PO Number\":\"111\",\"UNICEF section\":\"Health\"},\"id\":\"05934ae338431f28bf6793b24181ea5e\",\"date_created\":\"1520892000000\",\"_rev\":\"v1\"}],\"type\":\"Feature\",\"id\":\"90398\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[43.5978597,-14.1699446],[43.5978956,-13.1699609],[43.5978794,-13.1699947],[42.5978434,-14.1699784],[42.5978597,-14.1699446]]]},\"properties\":{\"uid\":\"3970790a-5a00-11ea-82b4-0242ac130003\",\"code\":\"21384421\",\"type\":\"Larval Dipping\",\"status\":\"Active\",\"parentId\":\"3724\",\"geographicLevel\":4,\"version\":0},\"serverVersion\":1542376382842,\"locationTags\":null}";
+	protected static DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd");
+
+	private static String physicalLocationAndStocksJsonObject =
+			"{\"stocks\":[{\"type\":\"Stock\",\"serverVersion\":1521112142815,\"identifier\":15,\"transaction_type\":\"Inventory\",\"providerid\":\"3c11c3ad-c366-443f-8ca8-c8c21f5bdd22\",\"value\":10,\"version\":1606748740346,\"deliveryDate\":\"2020-06-06\",\"accountabilityEndDate\":\"2020-07-02T10:00:00.000+0000\",\"donor\":\"ADB\",\"serialNumber\":\"1234serial\",\"locationId\":\"90398\",\"customProperties\":{\"PO Number\":\"111\",\"UNICEF section\":\"Health\"},\"id\":\"05934ae338431f28bf6793b241b3cd6f\",\"date_created\":\"1520892000000\",\"_rev\":\"v1\"},{\"type\":\"Stock\",\"serverVersion\":1521112142815,\"identifier\":1,\"transaction_type\":\"Inventory\",\"providerid\":\"3c11c3ad-c366-443f-8ca8-c8c21f5bdd22\",\"value\":10,\"version\":1606748740346,\"deliveryDate\":\"2020-06-06\",\"accountabilityEndDate\":\""
+					+ DateTime.now().plusDays(1).toLocalDateTime().toString(formatter)
+					+ "\",\"donor\":\"ADB\",\"serialNumber\":\"1234serial\",\"locationId\":\"90398\",\"customProperties\":{\"PO Number\":\"111\",\"UNICEF section\":\"Health\"},\"id\":\"05934ae338431f28bf6793b24181ea5e\",\"date_created\":\"1520892000000\",\"_rev\":\"v1\"}],\"type\":\"Feature\",\"id\":\"90398\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[43.5978597,-14.1699446],[43.5978956,-13.1699609],[43.5978794,-13.1699947],[42.5978434,-14.1699784],[42.5978597,-14.1699446]]]},\"properties\":{\"uid\":\"3970790a-5a00-11ea-82b4-0242ac130003\",\"code\":\"21384421\",\"type\":\"Larval Dipping\",\"status\":\"Active\",\"parentId\":\"3724\",\"geographicLevel\":4,\"version\":0},\"serverVersion\":1542376382842,\"locationTags\":null}";
 
 	@Test
 	public void testConvertLocationAndStocksToBundleResourceShouldReturnLocationStockBundle() {
@@ -43,6 +51,9 @@ public class PhysicalLocationAndStocksConverterTest {
 		assertTrue(PathEvaluatorLibrary.getInstance()
 				.evaluateBooleanExpression(bundle,
 						"Bundle.entry.resource.ofType(SupplyDelivery).identifier.where(system='isPastAccountabilityDate' and value='true').exists()"));
+		assertTrue(PathEvaluatorLibrary.getInstance()
+				.evaluateBooleanExpression(bundle,
+						"Bundle.entry.resource.ofType(SupplyDelivery).identifier.where(system='isPastAccountabilityDate' and value='false').exists()"));
 		System.out.println(bundle);
 	}
 }
