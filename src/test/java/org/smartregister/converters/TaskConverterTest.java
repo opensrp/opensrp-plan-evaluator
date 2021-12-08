1d21f0869a340772bf7f30e0d0f39cbbdd7f8eb7
hilpitome <hilpitome@gmail.com>
2021-12-08 10:44:33 +0300

    reset taskjson

--- src/test/java/org/smartregister/converters/TaskConverterTest.java
+++ src/test/java/org/smartregister/converters/TaskConverterTest.java
@@ -16,20 +16,19 @@ import static org.junit.Assert.assertNotNull;
 
 public class TaskConverterTest {
 	
-	private String taskJson = "{\"identifier\":\"tsk11231jh22\",\"planIdentifier\":\"IRS_2018_S1\",\"groupIdentifier\":\"2018_IRS-3734{\",\"status\":\"Ready\",\"businessStatus\":\"Not Visited\",\"priority\":\"routine\",\"code\":\"IRS\",\"description\":\"Spray House\",\"focus\":\"IRS Visit\",\"for\":\"location.properties.uid:41587456-b7c8-4c4e-b433-23a786f742fc\",\"executionPeriod\": {\"start\":\"2018-11-10T22:00\",\"end\":\"2019-11-10T21:00\"},\"authoredOn\":\"2018-10-31T07:00\",\"lastModified\":\"2018-10-31T07:00\",\"owner\":\"demouser\",\"note\":[{\"authorString\":\"demouser\",\"time\":\"2018-01-01T08:00\",\"text\":\"This should be assigned to patrick.\"}],\"serverVersion\":0,\"reasonReference\":\"reasonrefuuid\",\"location\":\"catchment1\",\"requester\":\"chw1\"}";
-	
+	private String taskJson = "{\"identifier\":\"tsk11231jh22\",\"planIdentifier\":\"IRS_2018_S1\",\"groupIdentifier\":\"2018_IRS-3734{\",\"status\":\"Ready\",\"businessStatus\":\"Not Visited\",\"priority\":\"routine\",\"code\":\"IRS\",\"description\":\"Spray House\",\"focus\":\"IRS Visit\",\"for\":\"location.properties.uid:41587456-b7c8-4c4e-b433-23a786f742fc\",\"executionPeriod\": {\"start\":\"2018-11-10T2200\",\"end\":\"2019-11-10T2100\"},\"authoredOn\":\"2018-10-31T0700\",\"lastModified\":\"2018-10-31T0700\",\"owner\":\"demouser\",\"note\":[{\"authorString\":\"demouser\",\"time\":\"2018-01-01T0800\",\"text\":\"This should be assigned to patrick.\"}],\"serverVersion\":0,\"reasonReference\":\"reasonrefuuid\",\"location\":\"catchment1\",\"requester\":\"chw1\"}";
 	private String dateFormat = "yyyy-MM-dd'T'HH:mm'Z'";
 	
-	private static Gson gson = new GsonBuilder().registerTypeAdapter(DateTime.class, new TaskDateTimeTypeConverter("yyyy-MM-dd'T'HH:mm'Z'"))
+	private static Gson gson = new GsonBuilder().registerTypeAdapter(DateTime.class, new TaskDateTimeTypeConverter())
 	        .serializeNulls().create();
 	
 	@Test
 	public void testConvertToFhirTask() {
 		org.smartregister.domain.Task task = gson.fromJson(taskJson, org.smartregister.domain.Task.class);
-//		DateTime testNotetime = task.getNotes().get(0).getTime();
-//		DateTimeZone zoneUTC = DateTimeZone.UTC;
-//		DateTime utcTime = testNotetime.withZone(zoneUTC);
-//		task.getNotes().get(0).setTime(utcTime);
+		DateTime testNotetime = task.getNotes().get(0).getTime();
+		DateTimeZone zoneUTC = DateTimeZone.UTC;
+		DateTime utcTime = testNotetime.withZone(zoneUTC);
+		task.getNotes().get(0).setTime(utcTime);
 		Task fihrTask = TaskConverter.convertTasktoFihrResource(task);
 		assertNotNull(fihrTask);
 		assertEquals(fihrTask.getStatus().getValueAsEnumConstant().value(),
