180435500fb3f77923d63294345d0d4ac06ac2d9
Allan O <a.onchuru@gmail.com>
2021-12-15 16:45:12 +0300

    :white_check_mark: Update PractitionerTest

--- src/test/java/org/smartregister/domain/PractitionerTest.java
+++ src/test/java/org/smartregister/domain/PractitionerTest.java
@@ -1,6 +1,7 @@
 package org.smartregister.domain;
 
 import junit.framework.TestCase;
+import org.joda.time.DateTime;
 import org.junit.Assert;
 
 public class PractitionerTest extends TestCase {
@@ -82,5 +83,47 @@ public class PractitionerTest extends TestCase {
 		practitioner.setUsername(username);
 
 		Assert.assertEquals(username, practitioner.getUsername());
+	}
+
+	public void testSetDateCreated() {
+		DateTime dateCreated = new DateTime();
+		practitioner.setDateCreated(dateCreated);
+
+		Assert.assertEquals(dateCreated, practitioner.getDateCreated());
+	}
+
+	public void testGetDateCreated() {
+		DateTime dateCreated = new DateTime();
+		practitioner.setDateCreated(dateCreated);
+
+		Assert.assertEquals(dateCreated, practitioner.getDateCreated());
+	}
+
+	public void testSetDateEdited() {
+		DateTime dateCreated = new DateTime();
+		practitioner.setDateEdited(dateCreated);
+
+		Assert.assertEquals(dateCreated, practitioner.getDateEdited());
+	}
+
+	public void testGetDateEdited() {
+		DateTime dateCreated = new DateTime();
+		practitioner.setDateEdited(dateCreated);
+
+		Assert.assertEquals(dateCreated, practitioner.getDateEdited());
+	}
+
+	public void testSetServerVersion() {
+		long serverVersion = 1;
+		practitioner.setServerVersion(serverVersion);
+
+		Assert.assertEquals(serverVersion, practitioner.getServerVersion());
+	}
+
+	public void testGetServerVersion() {
+		long serverVersion = 2;
+		practitioner.setServerVersion(serverVersion);
+
+		Assert.assertEquals(serverVersion, practitioner.getServerVersion());
 	}
 }
